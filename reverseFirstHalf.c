e0379ad338aadd96f9e0ed12cce200353156bc58
Rohit Kalubarme <67742127+Rohit1802@users.noreply.github.com>
2021-09-11 10:56:54 +0530

    Add files via upload

---
+++ reverseFirstHalf.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main(){
+	int a[100] ,n , i, j,temp;
+	//Taking the Size from user
+	printf("Enter the Size: ");
+	scanf("%d",&n);
+	
+	//Taking the Values from user
+	printf("Enter the values..");
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	
+	//Showing Existing Values
+	printf("The Values are ..");
+	for(i=0;i<n;i++){
+		printf("%d ",a[i]);
+	}
+	
+	//Reversing the first Half Values
+    for(i=0 ,j=(n-1)/2 ;i<n/4 ; i++ ,j--){
+    	temp = a[i];
+    	a[i] = a[j];
+    	a[j] = temp;
+	}
+	
+	//New elements are 
+	printf("The new Elements are..");
+	for(i=0;i<n;i++){
+		printf("%d ",a[i]);
+	}
+}
