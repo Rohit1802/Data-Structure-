589a37af0b1498a1c4d3a0a579e9463c4eef7df1
Rohit Kalubarme <67742127+Rohit1802@users.noreply.github.com>
2021-09-12 15:00:43 +0530

    Add files via upload

---
+++ Smallest.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	int a[50] ,i, n;
+	printf("Enter the size: ");
+	scanf("%d",&n);
+	
+	printf("Enter the elements :");
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	
+	for(i=1;i<n;i++){
+		if( a[0] > a[i]){
+			a[0] = a[i];
+		}
+	}
+	
+	printf("Smallest element is %d", a[0]);
+}
